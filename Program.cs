fe386e38b3d85532565c94aad847d179830dcf91
Bruce Alexsander <brucealexsanderalves@gmail.com>
2018-11-11 10:47:01 -0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace TestePratico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Busca os arquivos
+            string arquivo1 = System.IO.File.ReadAllText(@"C:\Temp\texto1.txt", Encoding.Default);
+
+            string arquivo2 = System.IO.File.ReadAllText(@"C:\Temp\texto2.txt", Encoding.Default);
+
+            var tratamento = new TratarTexto();
+
+            //faz o tratamento dos arquivos removendo coisas desnecessárias e criando um array
+            var texto1 = tratamento.RemoverCaracteresEspeciais(arquivo1)?.Split(' ');
+            var texto2 = tratamento.RemoverCaracteresEspeciais(arquivo2)?.Split(' ');                     
+
+            //Retorna os vocabulários
+            var vocabulario1 = tratamento.RetornarVocabularioPalavrasIsoladas(texto1, texto2);
+            var vocabulario2 = tratamento.RetornarVocabularioDuasPalavras(texto1, texto2);
+
+            //Retorna a frequencia das palavras do vocabulário em cada texto
+            var frequenciaVocabulario1NoTxt1 = tratamento.ObterFrequenciaVocabularioIsolado(texto1, vocabulario1);
+            var frequenciaVocabulario1NoTxt2 = tratamento.ObterFrequenciaVocabularioIsolado(texto2, vocabulario1);
+
+            var frequenciaVocabulario2NoTxt1 = tratamento.ObterFrequenciaVocabularioDuasPalavras(texto1, vocabulario2);
+            var frequenciaVocabulario2NoTxt2 = tratamento.ObterFrequenciaVocabularioDuasPalavras(texto2, vocabulario2);
+
+            //Formata os dados para a exibição e os exibe na tela
+            ExibirDadosNaTela(tratamento.FormatarDadosParaExibicao("Vocabulário 1", vocabulario1));
+            ExibirDadosNaTela(tratamento.FormatarDadosParaExibicao("Frequencia Vocabulário 1 no Texto 1", frequenciaVocabulario1NoTxt1));
+            ExibirDadosNaTela(tratamento.FormatarDadosParaExibicao("Frequencia Vocabulário 1 no Texto 2", frequenciaVocabulario1NoTxt2));
+
+            ExibirDadosNaTela(tratamento.FormatarDadosParaExibicao("Vocabulário 2", vocabulario2));
+            ExibirDadosNaTela(tratamento.FormatarDadosParaExibicao("Frequencia Vocabulário 2 no Texto 1", frequenciaVocabulario2NoTxt1));
+            ExibirDadosNaTela(tratamento.FormatarDadosParaExibicao("Frequencia Vocabulário 2 no Texto 2", frequenciaVocabulario2NoTxt2));
+
+            Console.ReadKey();
+
+            void ExibirDadosNaTela(string texto)
+            {
+                Console.WriteLine(texto);
+            }
+        }
+    }
+}
---
+++ TratarTexto.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TestePratico
+{
+    /// <summary>
+    /// Classe responsavel pelo tratamento dos textos
+    /// </summary>
+    public class TratarTexto
+    {
+        /// <summary>
+        /// Retorna o vocabulário de palavras isoladas
+        /// </summary>
+        /// <param name="texto1"></param>
+        /// <param name="texto2"></param>
+        /// <returns></returns>
+        public string[] RetornarVocabularioPalavrasIsoladas(string[] texto1, string[] texto2)
+        {
+            if (texto1.Count() == 0)
+                return null;
+
+            var retorno = new List<string>();
+
+            for (int i = 0; i < texto1.Length; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(texto1[i]) && VerificarSeEPalavra(texto1[i].ToCharArray()[0]))
+                {
+                    if (!retorno.Contains(texto1[i].ToLower()))
+                        retorno.Add(texto1[i].ToLower());
+                }
+            }
+
+            for (int i = 0; i < texto2.Length; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(texto2[i]) && VerificarSeEPalavra(texto2[i].ToCharArray()[0]))
+                {
+                    if (!retorno.Contains(texto2[i].ToLower()))
+                        retorno.Add(texto2[i].ToLower());
+                }
+            }
+
+            return retorno.ToArray();
+        }
+
+        /// <summary>
+        /// Retorna o vocabulário de duas palavras
+        /// </summary>
+        /// <param name="texto1"></param>
+        /// <param name="texto2"></param>
+        /// <returns></returns>
+        public string[] RetornarVocabularioDuasPalavras(string[] texto1, string[] texto2)
+        {
+            if (texto1.Count() == 0)
+                return null;
+
+            var retorno = new List<string>();
+
+            for (int i = 0; i < texto1.Length -1; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(texto1[i]) && VerificarSeEPalavra(texto1[i].ToCharArray()[0]))
+                {
+                    if (!retorno.Contains($"{texto1[i].ToLower()} {texto1[i + 1].ToLower()}"))
+                        retorno.Add($"{texto1[i].ToLower()} {texto1[i + 1].ToLower()}");
+                }
+            }
+
+            for (int i = 0; i < texto2.Length - 1; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(texto2[i]) && VerificarSeEPalavra(texto2[i].ToCharArray()[0]))
+                {
+                    if (!retorno.Contains($"{texto2[i].ToLower()} {texto2[i + 1].ToLower()}"))
+                        retorno.Add($"{texto2[i].ToLower()} {texto2[i + 1].ToLower()}");
+                }
+            }
+
+            return retorno.ToArray();
+        }
+
+        /// <summary>
+        /// Valida se a primeiro caractere do é uma letra
+        /// </summary>
+        /// <param name="texto"></param>
+        /// <returns></returns>
+        private bool VerificarSeEPalavra(char texto)
+        {
+            if (char.IsLetter(texto))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Remove os caracteres especiais do texto
+        /// </summary>
+        /// <param name="texto"></param>
+        /// <param name="permitirEspaco"></param>
+        /// <returns></returns>
+        public string RemoverCaracteresEspeciais(string texto, bool permitirEspaco = true)
+        {
+            string retorno;
+
+            texto = texto.Replace('-', ' ').Replace("  ", " ");
+
+            if (permitirEspaco)
+                retorno = System.Text.RegularExpressions.Regex.Replace(texto, @"[^0-9a-zA-ZéúíóáÉÚÍÓÁèùìòàÈÙÌÒÀõãñÕÃÑêûîôâÊÛÎÔÂëÿüïöäËYÜÏÖÄçÇ\s]+?", string.Empty);
+            else
+                retorno = System.Text.RegularExpressions.Regex.Replace(texto, @"[^0-9a-zA-ZéúíóáÉÚÍÓÁèùìòàÈÙÌÒÀõãñÕÃÑêûîôâÊÛÎÔÂëÿüïöäËYÜÏÖÄçÇ]+?", string.Empty);
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Retorna a frequencia do vocabulário isolado em um texto
+        /// </summary>
+        /// <param name="texto"></param>
+        /// <param name="vocabulario"></param>
+        /// <returns></returns>
+        public int[] ObterFrequenciaVocabularioIsolado(string[] texto, string[] vocabulario)
+        {
+            var retorno = new List<int>();
+
+            for (int i = 0; i < vocabulario.Length; i++)
+            {
+                var contador = 0;
+
+                for (int j = 0; j  < texto.Length; j ++)
+                {
+                    if (vocabulario[i] == texto[j].ToLower())
+                    {
+                        contador++;
+                    }
+                }
+
+                retorno.Add(contador);
+            }
+
+            return retorno.ToArray();
+        }
+
+        /// <summary>
+        /// Retorna a frequencia  do vocabulário de duas palavras em um texto
+        /// </summary>
+        /// <param name="texto"></param>
+        /// <param name="vocabulario"></param>
+        /// <returns></returns>
+        public int[] ObterFrequenciaVocabularioDuasPalavras(string[] texto, string[] vocabulario)
+        {
+            var retorno = new List<int>();
+
+            for (int i = 0; i < vocabulario.Length; i++)
+            {
+                var contador = 0;
+
+                for (int j = 0; j < texto.Length -1; j++)
+                {
+                    if (vocabulario[i] == $"{texto[j].ToLower()} {texto[j+1].ToLower()}")
+                    {
+                        contador++;
+                    }
+                }
+
+                retorno.Add(contador);
+            }
+
+            return retorno.ToArray();
+        }
+
+        /// <summary>
+        /// Formata os dados para a exibição
+        /// </summary>
+        /// <param name="descricao"></param>
+        /// <param name="dados"></param>
+        /// <returns></returns>
+        public string FormatarDadosParaExibicao(string descricao, string[] dados)
+        {
+            var retorno = string.Empty;
+
+            retorno = $"{descricao} - [{string.Join(",", dados)}]";
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Formata os dados para exibição
+        /// </summary>
+        /// <param name="descricao"></param>
+        /// <param name="dados"></param>
+        /// <returns></returns>
+        public string FormatarDadosParaExibicao(string descricao, int[] dados)
+        {
+            var retorno = string.Empty;
+
+            retorno = $"{descricao} - [{string.Join(",", dados)}]";
+
+            return retorno;
+        }
+    }
+}
